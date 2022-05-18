0fd6f0b7ba9ac2e592c773e9d9f66c555e48b343
teo009 <teo.obando09@gmail.com>
2022-05-18 16:06:10 -0600

    [CREATED] the project, installed tailwind and some config and expamples in index.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    './src/**/*{html,js,jsx}'
+  ],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
