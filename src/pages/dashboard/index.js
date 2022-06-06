37fa300eee39f8b222c7922655a6199d8e2d86c2
Teodoro Obando <teo.obando09@gmail.com>
2022-06-05 21:58:10 -0600

    [ADD] pagination not functional

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,24 +1,15 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 import endPoints from "@services/api";
 import useFetch from "@hooks/useFetch";
 
-const people = [
-  {
-    name: "Jane Cooper",
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
-    email: "jane.cooper@example.com",
-    image:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-];
-
 const PRODUCT_LIMIT = 5;
-const PRODUCT_OFFSET = 5; 
 
 export default function Dashboard() {
-  const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
-  //console.log(products);
+  const [page, setPage] = useState(1);
+  const [currentProducts, setCurrentProducts] = useState(null)  
+  const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, (page - 1) * PRODUCT_LIMIT));
+  const totalProducts = useFetch(endPoints.products.getProducts(0, 0)).length;
   return (
     <>
       <div className="flex flex-col">
@@ -106,6 +97,54 @@ export default function Dashboard() {
                   ))}
                 </tbody>
               </table>
+            </div>
+
+            <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+            <div className="flex-1 flex justify-between sm:hidden">
+              <a
+                href="/login"
+                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Previous
+              </a>
+              <a
+                href="/login"
+                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Next
+              </a>
+            </div>
+            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+              <div>
+                <p className="text-sm text-gray-700">
+                  Showing <span className="font-medium">{`${products.length}`}</span> to <span className="font-medium">10</span> of{' '}
+                  <span className="font-medium">{totalProducts}</span> results
+                </p>
+              </div>
+              <div>
+                <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+                  <button
+                    className='relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+                    onClick={() => {
+                      page == 1 ? setPage(page) : setPage(page - 1);
+                    }}
+                  >
+                    <span className='sr-only'>Previous</span>
+                    <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
+                  </button>
+                  <a aria-current='page' className='z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium'>
+                    {page}aa
+                  </a>
+                  <button
+                    className='relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+                    onClick={() => ((page + 1) * PRODUCT_LIMIT > totalProducts ? setPage(page) : setPage(page + 1))}
+                  >
+                    <span className='sr-only'>Next</span>
+                    <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
+                  </button>
+                </nav>
+              </div>
+            </div>
             </div>
           </div>
         </div>
