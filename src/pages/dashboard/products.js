e8c17b3aa0b53125794c91d1217a319119df05c8
teo009 <teo.obando09@gmail.com>
2022-06-06 16:26:12 -0600

    [ADD] our modal to dashboard products view
    Getting ready the list of products content

--- src/pages/dashboard/products.js
+++ src/pages/dashboard/products.js
@@ -1,121 +1,30 @@
 import { useState } from 'react';
-import { Menu, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
-import {
-  BriefcaseIcon,
-  CalendarIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  CurrencyDollarIcon,
-  LinkIcon,
-  LocationMarkerIcon,
-  PencilIcon,
-} from '@heroicons/react/solid'
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
+import { CheckIcon, PlusCircleIcon } from '@heroicons/react/solid';
+import Modal from '@common/Modal';
 
 export default function products() {
     const [products, setProducts] = useState([]);
+    const [open, setOpen] = useState(false);
     return (
         <>
-            <div className="lg:flex lg:items-center lg:justify-between">
+            <div className="lg:flex lg:items-center lg:justify-between mb-8">
                 <div className="flex-1 min-w-0">
-                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Back End Developer</h2>
-                    <div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-                        <div className="mt-2 flex items-center text-sm text-gray-500">
-                            <BriefcaseIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            Full-time
-                        </div>
-                        <div className="mt-2 flex items-center text-sm text-gray-500">
-                            <LocationMarkerIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            Remote
-                        </div>
-                        <div className="mt-2 flex items-center text-sm text-gray-500">
-                            <CurrencyDollarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            $120k &ndash; $140k
-                        </div>
-                        <div className="mt-2 flex items-center text-sm text-gray-500">
-                            <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            Closing on January 9, 2020
-                        </div>
-                    </div>
+                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">List of Products</h2>
                 </div>
-            <div className="mt-5 flex lg:mt-0 lg:ml-4">
-                <span className="hidden sm:block">
-                    <button
-                        type="button"
-                        className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        <PencilIcon className="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-                        Edit
-                    </button>
+                <div className="mt-5 flex lg:mt-0 lg:ml-4">
+                    <span className="sm:ml-3">
+                        <button
+                            onClick={() => setOpen(true)}
+                            type="button"
+                            className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            <PlusCircleIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                            Add Product
+                        </button>
                     </span>
-
-                    <span className="hidden sm:block ml-3">
-                    <button
-                        type="button"
-                        className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        <LinkIcon className="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-                        View
-                    </button>
-                </span>
-
-                <span className="sm:ml-3">
-                    <button
-                        type="button"
-                        className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        <CheckIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-                        Publish
-                    </button>
-                </span>
-
-                {/* Dropdown */}
-                <Menu as="div" className="ml-3 relative sm:hidden">
-                    <Menu.Button className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        More
-                        <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-                    </Menu.Button>
-
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                    >
-                        <Menu.Items className="origin-top-right absolute right-0 mt-2 -mr-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <Menu.Item>
-                                {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                >
-                                    Edit
-                                </a>
-                                )}
-                            </Menu.Item>
-                            <Menu.Item>
-                                {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                >
-                                    View
-                                </a>
-                                )}
-                            </Menu.Item>
-                        </Menu.Items>
-                    </Transition>
-                </Menu>
+                </div>
             </div>
-            </div>
-            {/* 
+             
             <div className="flex flex-col">
                 <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -208,70 +117,12 @@ export default function products() {
                                 </tbody>
                             </table>
                         </div>
-
-                        <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-                            <div className="flex-1 flex justify-between sm:hidden">
-                                <a
-                                    href="/login"
-                                    className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                                >
-                                    Previous
-                                </a>
-                                <a
-                                    href="/login"
-                                    className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                                >
-                                    Next
-                                </a>
-                            </div>
-                            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-                                <div>
-                                    <p className="text-sm text-gray-700">
-                                        Showing{" "}
-                                        <span className="font-medium">{`${products.length}`}</span>{" "}
-                                        to <span className="font-medium">10</span> of{" "}
-                                        <span className="font-medium">{totalProducts}</span> results
-                                    </p>
-                                </div>
-                                <div>
-                                    <nav
-                                        className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-                                        aria-label="Pagination"
-                                    >
-                                        <button
-                                            className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                                            onClick={() => {
-                                                page == 1 ? setPage(page) : setPage(page - 1);
-                                            }}
-                                        >
-                                            <span className="sr-only">Previous</span>
-                                            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-                                        </button>
-                                        <a
-                                            aria-current="page"
-                                            className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-                                        >
-                                            {page}aa
-                                        </a>
-                                        <button
-                                            className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                                            onClick={() =>
-                                                (page + 1) * PRODUCT_LIMIT > totalProducts
-                                                ? setPage(page)
-                                                : setPage(page + 1)
-                                            }
-                                        >
-                                            <span className="sr-only">Next</span>
-                                            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-                                        </button>
-                                    </nav>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
-            */}
+            <Modal open={open} setOpen={setOpen}>
+                <h1>Hello World</h1>
+            </Modal>
         </>
     );
 }
